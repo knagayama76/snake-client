2b3177015f72b95d098e08415b9e2fbb264acfbd
knagayama76 <knagayama76>
2022-01-19 15:26:00 -0500

    wrote my initial

--- client.js
+++ client.js
@@ -11,8 +11,9 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
-  conn.on("data", (data) => {
-    console.log(data);
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: KN");
   });
 
   return conn;
